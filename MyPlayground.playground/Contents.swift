68f42a693fe5d0b48334e386f0fc70cb6c5be7d8
Moisés Cervantes <cervantes.moises@me.com>
2015-07-28 21:55:37 -0500

    Inicio de Playgrounds

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//: Variables
+
+var str = "Hello"
+
+var name = "Moisés"
+
+name = "Mac"
+
+var int = 3
+
+3 * int
+
+var a = 5
+
+var b = a * 3
+
+var c:Double = 5 / 2
+
+c * Double(int)
+
+var longSentence = str + ", " + name
+
+var newString = "Mi gato tiene \(int) años."
+
+var x = 3.5
+
+var y = 4
+
+var z = x * Double(y)
+
+var suma = "\(x) por \(y) es igual a \(z)"
+
+//: Arreglos
+
+
